af30d7e882b85ba07cbd14447f26d72e828c8ddc
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-06-14 19:14:02 +0630

    chore: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +17,9 @@ export default defineConfig({
 			},
 		},
 	},
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
 
